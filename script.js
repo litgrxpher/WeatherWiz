38d865c642a0443bb5849227919b1b88023c6d0a
Adharsh CS <84709739+litgrxpher@users.noreply.github.com>
2024-06-27 19:05:58 +0530

    push

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.searchbox button');
+const weatherbox = document.querySelector('.weather-box');
+const weatherdetails = document.querySelector('.weather-details');
+
+search.addEventListener('click', ()=>{
+
+    const APIKey = '415b82464c81128ebe920997d52ef0a6';
+    const city = document.querySelector('.searchbox input').ariaValueMax;
+})
+
